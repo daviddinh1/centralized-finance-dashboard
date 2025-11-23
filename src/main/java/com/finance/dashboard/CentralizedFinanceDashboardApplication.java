464733008f889e956faf7f159d3671348f198566
daviddinh1 <daviddinh602680@gmail.com>
2025-11-23 17:06:59 -0600

    Initial commit

---
+++ src/main/java/com/finance/dashboard/CentralizedFinanceDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.finance.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedFinanceDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedFinanceDashboardApplication.class, args);
+	}
+
+}
