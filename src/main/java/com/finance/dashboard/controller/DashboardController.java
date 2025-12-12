6d076586fa7d414e7f01e995fd75164cd9fd35b6
daviddinh1 <daviddinh602680@gmail.com>
2025-12-12 17:09:33 -0600

    feature: created route for dashbaord for getting dashboardDTO

---
+++ src/main/java/com/finance/dashboard/controller/DashboardController.java
@@ -0,0 +1,26 @@
+package com.finance.dashboard.controller;
+
+import com.finance.dashboard.dto.response.DashboardDTO;
+import com.finance.dashboard.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<DashboardDTO> getDashboard(@AuthenticationPrincipal UserDetails userDetails) {
+        String userId = userDetails.getUsername();
+        DashboardDTO dashboardDTO = dashboardService.getDashboard(userId);
+        return ResponseEntity.ok(dashboardDTO);
+    }
+}
