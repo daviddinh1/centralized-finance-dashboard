0493a890b61d31f8b1ddd64a4c6f6b1c8e0e59ef
daviddinh1 <daviddinh602680@gmail.com>
2025-12-12 17:25:56 -0600

    refactor: got rid of unused import

--- src/main/java/com/finance/dashboard/controller/DashboardController.java
+++ src/main/java/com/finance/dashboard/controller/DashboardController.java
@@ -3,7 +3,6 @@ package com.finance.dashboard.controller;
 import com.finance.dashboard.dto.response.DashboardDTO;
 import com.finance.dashboard.service.DashboardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
